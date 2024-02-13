7fccbcbf5d9315343a9d8ea6522d22a5a22a3498
name <usymmij@gmail.com>
2024-02-13 14:11:50 -0500

    refactored app

--- controls/src/app/page.tsx
+++ controls/src/app/page.tsx
@@ -1,113 +1,197 @@
 import Image from "next/image";
+'use client'
+import { useEffect, useState } from "react";
+
+const throttleRamp = 0.05;
+const steerRamp = 0.07;
+
+const data = {
+  speed: 0, // [-100, 100] 
+  turn: 0,  // [-100, 100]
+  aim: 0,   // [-1, 1]
+  shoot: 0, // {0, 1}
+  shootSpeed: 0  // [0, 100]
+}
 
 export default function Home() {
+
+  const [throttle, setThrottle] = useState(0);
+  const [steer, setSteer] = useState(0);
+  const [aim, setAim] = useState(0);
+  const [shootSpeed, setShootSpeed] = useState(0);
+  const [shoot, setShoot] = useState(false);
+  const [currIter, nextiter] = useState(false);
+
+
+  function driveFuncs(key: string, down: boolean): void {
+    switch (key) {
+      case 'w':
+        if (down) setThrottle(1);
+        if (!down && throttle == 1) setThrottle(0);
+        break;
+      case 's':
+        if (down) setThrottle(-1);
+        if (!down && throttle == -1) setThrottle(0);
+        break;
+      case 'a':
+        if (down) setSteer(-1);
+        if (!down && steer == -1) setSteer(0);
+        break;
+      case 'd':
+        if (down) setSteer(1);
+        if (!down && steer == 1) setSteer(0);
+        break;
+      case 'j':
+        if (down) setAim(-1);
+        if (!down && aim == -1) setAim(0);
+        break;
+      case 'k':
+        if (down) setAim(1);
+        if (!down && aim == 1) setAim(0);
+        break;
+      case 'h':
+        if (down) setShoot(true);
+        if (!down) setShoot(false);
+        break;        
+    }
+  }
+  useEffect(() => {
+    setTimeout(() => {
+      nextiter(!currIter)
+    }, 5)
+
+    if (throttle > 0) data.speed += (100 - data.speed) * throttleRamp;
+    else if (throttle < 0) data.speed += (-100 - data.speed) * throttleRamp;
+    else data.speed += (-data.speed) * throttleRamp;
+
+    if (steer > 0) data.turn += (100 - data.turn) * steerRamp;
+    else if (steer < 0) data.turn += (-100 - data.turn) * steerRamp;
+    else data.turn += (-data.turn) * steerRamp;
+    
+    data.speed = Number(data.speed.toFixed(6));
+    data.turn = Number(data.turn.toFixed(6));
+
+    data.shoot = shoot ? 1 : 0;
+
+    data.aim = aim;
+
+    data.shootSpeed = shootSpeed;
+
+    fetch(
+      "http://127.0.0.1:8000",
+      {
+        method: "post",
+        mode: "no-cors",
+        headers: {
+          "Content-Type": "application/text",
+        },
+        body: JSON.stringify(data)
+      }
+    ).then(() => {}).catch(() => {});
+
+  }, [currIter])
+
+  function keybinds(event: React.KeyboardEvent): void {
+    driveFuncs(event.key.toLowerCase(), event.type == "keydown");
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1"
+          />
+        </head>
+        <body onKeyDown={keybinds} role='presentation' onKeyUp={keybinds}>
+          <div className="w3-display-container w3-container w3-mobile" style={{ height: '100%', width: '100%', backgroundColor: "rgb(50,50,50)", maxWidth: "100%" }}>
+            <div className="w3-display-topmiddle" style={{ height: '44px', width: '90%' }}>
+              <div className="" style={{ height: '20px' }}></div>
+              <div className="w3-border" style={{ height: '24px', width: '100%' }}>
+                <div style={{
+                  height: '22px', width: Math.abs(data.speed) + '%',
+                  backgroundColor: `rgb(${255 * (data.speed > 0 ? 0 : 1)},0 ,${200 * (data.speed > 0 ? 1 : 0)})`
+                }}></div>
+              </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+              <div className="w3-border" style={{ height: '24px', width: '100%', display: 'flex' }}>
+                <div style={{
+                  height: '22px', width: `${data.turn < 0 ? (100 + data.turn) / 2 : 50}%`,
+                }}></div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                <div style={{
+                  height: '22px', width: `${data.turn < 0 ? -data.turn / 2 : 0}%`,
+                  backgroundColor: `rgb(3,3,3)`
+                }}></div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                <div style={{
+                  height: '22px', width: `${data.turn > 0 ? data.turn / 2 : 0}%`,
+                  backgroundColor: `rgb(3,3,3)`
+                }}></div>
+              </div>
+            </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+
+            <div className="w3-display-left w3-container" id='power' style={{ width: '50%', padding: '0px', maxWidth: '400px' }}>
+              <div className='w3-container' style={{ width: '100%', padding: '0px' }}>
+                <div className='w3-row' style={{ width: '100%', display: 'flex' }} >
+                  <div className='w3-col w3-container' style={{ maxWidth: '4em', width: '2em', flexGrow: '1' }}></div>
+                  <div className='w3-col w3-third w3-container' style={{ padding: '0px', flexGrow: '0' }} >
+                    <button className="w3-circle w3-ripple w3-grey" id="w" style={{ height: "5em", width: "5em", padding: '0px' }}> W </button>
+                  </div>
+                  <div className='w3-col w3-third w3-container' style={{ padding: '0px' }}>
+                    <button className="w3-circle w3-ripple w3-grey" id="k" style={{ height: "5em", width: "5em", padding: '0px' }}> K </button>
+                  </div>
+                </div>
+                <div className='w3-row' style={{ height: '0.5em', width: '100%', display: 'flex' }} ></div>
+                <div className='w3-row' style={{ height: '5em', width: '100%', display: 'flex' }} >
+                  <div className='w3-col w3-container' style={{ maxWidth: '4em', width: '2em', flexGrow: '1' }}></div>
+                  <div className='w3-col w3-third w3-container' style={{ padding: '0px' }} >
+                    <button className="w3-circle w3-ripple w3-grey" id="s" style={{ height: "5em", width: "5em", padding: '0px' }}> S </button>
+                  </div>
+                  <div className='w3-col w3-third w3-container' style={{ padding: '0px' }}>
+                    <button className="w3-circle w3-ripple w3-grey" id="j" style={{ height: "5em", width: "5em", padding: '0px' }}> J </button>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="w3-display-right w3-container" id='steering' style={{ width: '50%', padding: '0px', maxWidth: '400px' }}>
+            <div className='w3-container' style={{ width: '100%', padding: '0px' }}>
+              <div className='w3-row' style={{ height: '5em', width: '100%', display: 'flex' }} ></div>
+              <div className='w3-row' style={{ height: '5em', width: '100%', display: 'flex', marginLeft: 'auto', alignItems: 'flex-end' }}>
+                <div className='w3-col w3-third w3-container' style={{ padding: '0px' }} >
+                  <button className="w3-circle w3-ripple w3-grey w3-col w3-third w3-right" id="a" style={{ height: "5em", width: "5em", textAlign: 'center', padding: '0px' }}> A </button>
+                </div>
+                <div className='w3-col w3-third w3-container' style={{ padding: '0px' }}>
+                  <button className="w3-circle w3-ripple w3-grey w3-col w3-third w3-right" id="d" style={{ height: "5em", width: "5em", textAlign: 'center', padding: '0px' }}> D </button>
+                </div>
+                <div className='w3-col w3-container' style={{ width: '2em', flexGrow: '1' }}></div>
+              </div>
+              <div className='w3-row' style={{ height: '1em' }} ></div>
+              <div className='w3-row' style={{ display: 'flex', marginLeft: 'auto' }}>
+                <div className='w3-col w3-container' style={{ width: '4em', flexGrow: '1' }}></div>
+                <div className='w3-col w3-quarter w3-container' style={{ padding: '0px', flexGrow: '0.5' }}>
+                  <button className="w3-circle w3-ripple w3-grey" id="h" style={{ height: "5em", width: "5em", padding: '0px' }}> H </button>
+                </div>
+                <div className='w3-col w3-container' style={{ width: '2em', flexGrow: '1' }}></div>
+              </div>
+            </div>
+          </div>
+
+          <div className="w3-display-bottommiddle w3-container" style={{ width: '75%' }}>
+            <div className='w3-col'>
+              <input type="range" min="0" max="100" defaultValue="0" className="slider" onChange={e => setShootSpeed(Number(e.target.value))} id="myRange" style={{ width: '100%' }} />
+              <div className="w3-row" style={{ height: '20px' }}></div>
+            </div>
+          </div>
+        </body>
+      </html >
+    </>
   );
 }
