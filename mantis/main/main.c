6ef63881e4ae6a8d900deba02d960b8db94b71db
name <usymmij@gmail.com>
2024-02-14 03:51:57 -0500

    driver: basic multiple pwm channel control (up to 12)

--- mantis/main/main.c
+++ mantis/main/main.c
@@ -11,18 +11,27 @@ void app_main(void)
    * will not move if in this range
    */
 
-  int motors[] = {0};
+  int motors[] = {0, 2, 4};
   printf("dasdsadskdkasdskad %d \n", sizeof(motors) / 4); 
   initMotorControl(motors, sizeof(motors)/4);
 
   double val = 0;
+  double val2 = 0;
   double diff = 0.05;
+  double diff2 = 0.2;
+  //just some testing
   while(1) {
     setPWMSignal(0, val, 697, 2310);
+    setPWMSignal(4, val2, 697, 2310);
     vTaskDelay(pdMS_TO_TICKS(300));
     val += diff;
     if(val >= 1) diff -= 0.1;
     if(val <= -1) diff += 0.1;
+  
+    val2 += diff2;
+    if(val2 >= 1) diff2 -= 0.4;
+    if(val2 <= -1) diff2 += 0.4;
+
   }
   
   
--- mantis/main/servo.c
+++ mantis/main/servo.c
@@ -18,21 +18,20 @@ static const char *LOGTAG = "PWM Driver Logging";
 #define SERVO_TIMEBASE_RESOLUTION_HZ 1000000  // 1MHz, 1us per tick
 #define SERVO_TIMEBASE_PERIOD        20000    // 20000 ticks, 20ms
 #define MAX_MOTORS                   16
+// i "limit" the motors to 16, although the real limit is is the mcpwm timers, as there are 3 timers for each of the two groups
+// each timer can have 3 operators, which has 2 comparators, which has 2 generators
 
 #define INIT_VAL   1503     // the pulse width (in usecs) to initialize
                             // should it be a parameter? yes
                             // am I going to do that? no im tired and my head hurts
 
-mcpwm_cmpr_handle_t initMotor(int port) 
+mcpwm_timer_handle_t newtimer(int groupid)
 {
-    // check the mcpwm docs on esp i dont really understand this tbh
-    // init timer stuf
-    ESP_LOGI(LOGTAG, "Create timer and operator for group ");
-    printf("%i\n", port);
+    ESP_LOGI(LOGTAG, "Create timer");
 
     mcpwm_timer_handle_t timer = NULL;             
     mcpwm_timer_config_t timer_config = {
-        .group_id = port,
+        .group_id = groupid,
         .clk_src = MCPWM_TIMER_CLK_SRC_DEFAULT,
         .resolution_hz = SERVO_TIMEBASE_RESOLUTION_HZ,
         .period_ticks = SERVO_TIMEBASE_PERIOD,
@@ -40,9 +39,26 @@ mcpwm_cmpr_handle_t initMotor(int port)
     };
     ESP_ERROR_CHECK(mcpwm_new_timer(&timer_config, &timer));
 
+    return timer;
+}
+
+void enabletimer(mcpwm_timer_handle_t timer)
+{
+    ESP_LOGI(LOGTAG, "Enable and start timer");
+    ESP_ERROR_CHECK(mcpwm_timer_enable(timer));
+    ESP_ERROR_CHECK(mcpwm_timer_start_stop(timer, MCPWM_TIMER_START_NO_STOP));
+
+}
+
+mcpwm_cmpr_handle_t initMotor(int port, int groupid, mcpwm_timer_handle_t timer) 
+{
+    // check the mcpwm docs on esp i dont really understand this tbh
+    // init timer stuf
+   
+    ESP_LOGI(LOGTAG, "Create operator");
     mcpwm_oper_handle_t oper = NULL;
     mcpwm_operator_config_t operator_config = {
-        .group_id = port, // operator must be in the same group to the timer
+        .group_id = groupid, // operator must be in the same group to the timer
     };
     ESP_ERROR_CHECK(mcpwm_new_operator(&operator_config, &oper));
 
@@ -72,11 +88,6 @@ mcpwm_cmpr_handle_t initMotor(int port)
     ESP_ERROR_CHECK(mcpwm_generator_set_action_on_compare_event(generator,
                                                                 MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, comparator, MCPWM_GEN_ACTION_LOW)));
 
-    ESP_LOGI(LOGTAG, "Enable and start timer");
-    ESP_ERROR_CHECK(mcpwm_timer_enable(timer));
-    ESP_ERROR_CHECK(mcpwm_timer_start_stop(timer, MCPWM_TIMER_START_NO_STOP));
-
-
     return comparator;
 }
 
@@ -84,17 +95,32 @@ mcpwm_cmpr_handle_t comparators[MAX_MOTORS]; // we support up to 16 motors, can 
 int ports[MAX_MOTORS]; // same for ports
 int maxcmpr = 0;
 
+
 void initMotorControl(int motors[], int motorssize)
 {
+    int group = 0;
+    mcpwm_timer_handle_t timer = NULL;
+    int i=0;
 
- 
- 
-    for(int i=0; i < motorssize; i++)
+    // there are 2 timer groups
+    // each group has 3 timers / operators
+    // and each timer/oper combo has 2 comparators
+    // each comparator does have more generator, but we dont need that in our use case
+
+    for(i=0; i < motorssize; i++)
     {
-        mcpwm_cmpr_handle_t compar = initMotor(motors[i]);
+        if(i % 2 == 0) timer = newtimer(group);
+        mcpwm_cmpr_handle_t compar = initMotor(motors[i], group, timer);
+        
         ports[maxcmpr] = motors[i];
         comparators[maxcmpr++] = compar;
+
+        if(i % 2 == 1) enabletimer(timer);
+        if(i % 6 == 5) group += 1; // maximum of 12 motors (0 -> 11)
     }
+
+    if(i % 2 > 0) enabletimer(timer); // if the # of timers is not 2 mod 0
+
 }
 
 /*
@@ -113,8 +139,6 @@ void setPWMSignal(int port, double value, int min_us, int max_us) // ima copy wh
     if(i >= MAX_MOTORS) return;
     uint32_t comparedpulse = ((value + 1) / 2) * (max_us - min_us) + min_us;
     ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparators[i], comparedpulse));
- 
-    printf("uhh %f\n", value);
 
 }
 /*
